a5057794d5c7273dbd70c802c34b56572950c114
stellar <1828754519@qq.com>
2026-04-15 11:49:09 +0800

    chore:在Application.cpp.Run()创建VAOVBOVEO使用Nvidia自带的着色器尝试绘制了一个静态三角形

--- Hazel/src/Hazel/Core/Application.cpp
+++ Hazel/src/Hazel/Core/Application.cpp
@@ -38,6 +38,42 @@ Application::Application()
     // 这里不再使用 unique_ptr 或者shared_ptr 持有 ImGuiLayer避免双重释放
     m_ImGuiLayer = new ImGuiLayer();
     PushOverlay(m_ImGuiLayer);
+
+    // 创建 VAO 负责与着色器的顶点属性对接
+    glGenVertexArrays(1, &m_VertexArray);
+    glBindVertexArray(m_VertexArray);
+
+    // 创建 VBO 数据本体
+    glGenBuffers(1, &m_VertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+
+    // clang-format off
+    float vertices[3 * 3] = 
+    {
+        -0.5f, -0.5f, 0.0f, // left
+         0.5f, -0.5f, 0.0f, // right
+         0.0f,  0.5f, 0.0f  // top
+    };
+    // clang-format on
+    // 把顶点数据上传到 GPU
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // 设置顶点属性指针
+    glEnableVertexAttribArray(0);
+
+    // 参数说明：
+    // 0：顶点属性位置（location = 0）
+    // 3：每个顶点属性由 3 个 float 组成
+    // GL_FALSE：不需要归一化
+    // 3 * sizeof(float)：每个顶点属性占用的字节数（步长）
+    // (const void*)0：顶点属性数据在缓冲中的偏移量，这里是从头开始
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (const void*)0);
+
+    // 创建 EBO 索引缓冲对象
+    glGenBuffers(1, &m_IndexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+    unsigned int indices[3] = {0, 1, 2};
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 Application::~Application()
 {
@@ -80,14 +116,16 @@ void Application::Run()
 
     while (m_Running)
     {
-        // 这里的 glClearColor / glClear 是当前阶段最基础的 OpenGL 调用
-        // 主要用于验证：
-        // 1. GLFW 成功创建了窗口
-        // 2. OpenGL context 已成功建立
-        // 3. GLAD 已成功加载 OpenGL 函数指针
-        glClearColor(0.0f, 1.0f, 1.0f, 1);
+        glClearColor(0.1f, 0.1f, 0.1f, 1);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBindVertexArray(m_VertexArray);
+        // 参数说明：
+        // GL_TRIANGLES：绘制模式，表示每三个顶点构成一个三
+        // 3：索引数量，这里是 3 个顶点
+        // GL_UNSIGNED_INT：索引数据类型，这里是 unsigned int
+        // nullptr：索引数据在 EBO 中的偏移量，这里是从头开始
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
         // 先更新普通 Layer
         for (Layer* layer : m_LayerStack)
         {
